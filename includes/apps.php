830b7b82e4d93a92d3c4e109a56b49495b1d83af
Jay Wood <jerry@webdevstudios.com>
2016-05-05 00:56:39 -0400

    Working email sending

--- includes/apps.php
+++ includes/apps.php
@@ -21,11 +21,38 @@ class MS_Apps {
 		add_action( 'wp_ajax_minecraft-server-suite', array( $this, 'handle_ajax' ) );
 		add_action( 'admin_menu', array( $this, 'add_pending_apps_menu' ) );
 		add_filter( 'gettext', array( $this, 'publish_to_approve' ), 10, 2 );
+		add_action( 'publish_mc-applications', array( $this, 'send_approved_email' ), 10, 2 );
+	}
+
+	/**
+	 * @param int $post_id
+	 * @param WP_Post $post
+	 *
+	 * @author JayWood
+	 */
+	public function send_approved_email( $post_id, $post ) {
+
+		if ( 'mc-applications' !== $post->post_type ) {
+			return;
+		}
+
+		$user_email = get_post_meta( $post_id, 'email', true );
+		$username = get_post_meta( $post_id, 'mc-username', true );
+		if ( empty( $user_email ) || empty( $username ) ) {
+			return;
+		}
+
+		$subject = sprintf( __( 'Whitelist Application Approved: %s', 'minecraft-suite' ), $username );
+		$to[] = sprintf( '%s <%s>', $username, $user_email );
+		$message = get_option( 'mcs_email-body', '' );
+		$headers[] = '';
+
+		wp_mail( $to, $subject, $message );
 	}
 
 	public function handle_ajax() {
 		if ( ! wp_verify_nonce( $_POST['_wpnonce'], 'mcs-nonce' ) ) {
-			wp_send_json_error( __( 'Internal Server Error', 'ms' ) );
+			wp_send_json_error( __( 'Internal Server Error', 'minecraft-suite' ) );
 		}
 
 		$age = absint( $_POST['age'] );
@@ -35,34 +62,34 @@ class MS_Apps {
 		$server = esc_attr( $_POST['server-id'] );
 
 		if ( ! array_key_exists( $server, $this->_get_available_servers() ) ) {
-			wp_send_json_error( __( 'The specified server does not exist, please select a valid server.', 'ms' ) );
+			wp_send_json_error( __( 'The specified server does not exist, please select a valid server.', 'minecraft-suite' ) );
 		}
 
 		if ( $age > 100 ) {
-			wp_send_json_error( __( 'You are WAY too old to be playing Minecraft!!!', 'ms' ) );
+			wp_send_json_error( __( 'You are WAY too old to be playing Minecraft!!!', 'minecraft-suite' ) );
 		}
 
 		if ( $age < $this->get_minimum_age() ) {
-			wp_send_json_error( __( 'You do not meet the minimum age requirements to play on this server.', 'ms' ) );
+			wp_send_json_error( __( 'You do not meet the minimum age requirements to play on this server.', 'minecraft-suite' ) );
 		}
 
 		if ( empty( $email ) ) {
-			wp_send_json_error( __( 'Please enter a valid email address', 'ms' ) );
+			wp_send_json_error( __( 'Please enter a valid email address', 'minecraft-suite' ) );
 		}
 
 		if ( empty( $age ) || empty( $email ) || empty( $username ) || empty( $reason ) ) {
-			wp_send_json_error( __( 'All fields are required.', 'ms' ) );
+			wp_send_json_error( __( 'All fields are required.', 'minecraft-suite' ) );
 		}
 
 
 		if ( false !== $status = $this->user_has_application( $username ) ) {
-			wp_send_json_error( sprintf( __( 'You already have an active application with a status of [ %s ]', 'ms' ), $status ) );
+			wp_send_json_error( sprintf( __( 'You already have an active application with a status of [ %s ]', 'minecraft-suite' ), $status ) );
 		}
 
 		$insert_results = wp_insert_post( array(
 			'post_type' => 'mc-applications',
 			'post_status' => 'pending',
-			'post_title' => sprintf( __( 'Whitelist Application for: %s', 'ms' ), $username ),
+			'post_title' => sprintf( __( 'Whitelist Application for: %s', 'minecraft-suite' ), $username ),
 			'post_content' => $reason,
 		), true );
 
@@ -73,10 +100,10 @@ class MS_Apps {
 
 			wp_set_object_terms( $insert_results, intval( $server ), 'server', false );
 
-			wp_send_json_success( __( 'Success! - Your application is now pending approval by a member of the team. You will receive an email when your application has been reviewed along with our decision.', 'ms' ) );
+			wp_send_json_success( __( 'Success! - Your application is now pending approval by a member of the team. You will receive an email when your application has been reviewed along with our decision.', 'minecraft-suite' ) );
 		}
 
-		wp_send_json_error( __( 'An unspecified error occurred, please contact an admin, or try again later', 'ms' ) );
+		wp_send_json_error( __( 'An unspecified error occurred, please contact an admin, or try again later', 'minecraft-suite' ) );
 
 	}
 
