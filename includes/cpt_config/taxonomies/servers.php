ebac0ef9b2bf79bf469e4629b76f245a291c8768
Jay Wood <jerry@webdevstudios.com>
2017-03-26 19:27:09 -0400

    Store server IDs against term meta.

--- includes/cpt_config/taxonomies/servers.php
+++ includes/cpt_config/taxonomies/servers.php
@@ -8,4 +8,63 @@ class MS_Servers extends Taxonomy_Core {
 			'server',
 		), array(), array( 'mc-applications' ) );
 	}
+
+	public function hooks() {
+		if ( minecraft_suite()->is_multicraft_enabled() ) {
+			add_action( $this->taxonomy . '_edit_form_fields', array( $this, 'edit_form_fields' ) );
+			add_action( $this->taxonomy . '_add_form_fields', array( $this, 'add_form_fields' ) );
+
+			add_action( 'created_' . $this->taxonomy, array( $this, 'save_term_meta' ), 10, 2 );
+			add_action( 'edited_' . $this->taxonomy, array( $this, 'save_term_meta' ), 10, 2 );
+
+			add_filter( 'manage_edit-' . $this->taxonomy . '_columns', array( $this, 'tax_columns' ) );
+			add_filter( 'manage_' . $this->taxonomy . '_custom_column', array( $this, 'tax_col_content' ), 10, 3 );
+		}
+	}
+
+	public function tax_col_content( $content, $col_name, $term_id ) {
+		if ( 'server_id' == $col_name ) {
+			$server_id = get_term_meta( $term_id, 'server_id', true );
+
+			return absint( $server_id );
+		}
+
+		return $content;
+	}
+
+	public function tax_columns( $cols ) {
+
+		$cols['server_id'] = __( 'Server ID', 'minecraft-suite' );
+
+		return $cols;
+	}
+
+	public function save_term_meta( $term_id, $tag_id ) {
+		if ( isset( $_POST['server_id'] ) ) {
+			update_term_meta( $term_id, 'server_id', absint( $_POST['server_id'] ) );
+		}
+	}
+
+	public function edit_form_fields( $term ) {
+		$server_id = get_term_meta( $term->term_id, 'server_id', true );
+		?>
+		<tr class="form-field term-group-wrap">
+			<th scope="row">
+				<label for="server_id"><?php _e( 'Multicraft Server ID', 'minecraft-suite' ); ?></label>
+			</th>
+			<td>
+				<input type="text" id="server_id" name="server_id" value="<?php echo absint( $server_id ); ?>" />
+			</td>
+		</tr>
+		<?php
+	}
+
+	public function add_form_fields() {
+		?>
+		<div class="form-field term-group">
+			<label for="server_id"><?php _e( 'Multicraft Server ID', 'minecraft-suite' ); ?></label>
+			<input type="text" id="server_id" name="server_id" />
+		</div>
+		<?php
+	}
 }
